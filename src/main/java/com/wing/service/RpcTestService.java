2e2a5b6ef514b3c83eae73c45d979a8eb0072347
hai99long <hai99long@163.com>
2017-10-11 14:58:47 +0800

    增加注释，优化服务端接口请求的接口

---
+++ src/main/java/com/wing/service/RpcTestService.java
@@ -0,0 +1,9 @@
+package com.wing.service;
+
+import java.io.CharConversionException;
+import java.io.IOException;
+
+public interface RpcService {
+	public String strConcat(String requestStr, long no) throws IOException, CharConversionException;
+	public Integer 
+}
---
+++ src/main/java/com/wing/service/model/RpcTestModel.java
@@ -0,0 +1,7 @@
+package com.wing.service.model;
+
+/**
+ * Created by wanghl on 2017/10/11.
+ */
+public class RpcModel {
+}
